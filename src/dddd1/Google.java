720dfc897886b82680403700d5c1354054529bbb
leeabhishek12 <abhishekanu.sharma7@gmail.com>
2017-09-28 16:58:55 +0530

    change logger

--- src/dddd1/Google.java
+++ src/dddd1/Google.java
@@ -36,7 +36,8 @@ public class Google {
 	 
 	        
 	        driver.findElement(By.name("q")).sendKeys("Selenium");
-	        logger.info("keyword type");           
+	        logger.info("keyword type");        
+	        logger.info("keyword type");    
 	}
 
 }
